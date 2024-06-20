e1e973b1e84cbad6cb6579c95e255605a8d8c7f6
felix <90296853+yoo040501@users.noreply.github.com>
2024-06-20 22:19:02 +0900

    Update main.c

--- philo/philo/srcs/main.c
+++ philo/philo/srcs/main.c
@@ -47,3 +47,46 @@ int	main(int ac, char **av)
 	
 	return (EXIT_SUCCESS);
 }
+
+int	init_argv(t_arg *arg, int argc, char **argv)
+{
+	arg->philo_num = ft_atoi(argv[1]);
+	arg->life_time = ft_atoi(argv[2]);
+	arg->eat_time = ft_atoi(argv[3]);
+	arg->sleep_time = ft_atoi(argv[4]);
+	arg->monitor = 0;
+	if (arg->philo_num <= 0 || arg->life_time < 0 || \
+		arg->eat_time < 0 || arg->sleep_time < 0)
+		return (1);
+	if (argc == 6)
+	{
+		arg->eat_num = ft_atoi(argv[5]);
+		if (arg->eat_num <= 0)
+			return (1);
+	}
+	else
+		arg->eat_num = 0;
+	return (0);
+}
+
+int	init_philo(t_philo **philo, t_arg *arg)
+{
+	int		i;
+
+	i = 0;
+	*philo = malloc(sizeof(t_philo) * arg->philo_num);
+	if (!philo)
+		return (1);
+	while (i < arg->philo_num)
+	{
+		(*philo)[i].id = i + 1;
+		(*philo)[i].eat_count = 0;
+		(*philo)[i].left = i;
+		(*philo)[i].right = (i + 1) % arg->philo_num;
+		(*philo)[i].last_eat = 0;
+		(*philo)[i].last_time = 0;
+		(*philo)[i].arg = arg;
+		i++;
+	}
+	return (0);
+}
