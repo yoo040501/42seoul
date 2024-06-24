5e5d3a2b6ee6153bab73ac141da85a1026706640
felix <90296853+yoo040501@users.noreply.github.com>
2024-06-24 21:12:22 +0900

    Add files via upload

--- philo/philo/srcs/utils.c
+++ philo/philo/srcs/utils.c
@@ -1,22 +1,60 @@
-#include "../include/philo.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_utils.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpaeng <gpaeng@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/01 13:26:58 by gpaeng            #+#    #+#             */
+/*   Updated: 2021/08/03 15:16:20 by gpaeng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	error_message(const char message)
+#include "../includes/philo.h"
+
+int	ft_isspace(char c)
 {
-	printf("%s\n", message);
-	return (EXIT_FAILURE);
+	return (c == '\f' || c == '\n' || c == '\r'
+		|| c == '\t' || c == '\v' || c == ' ');
 }
 
 int	ft_atoi(char *str)
 {
-	int	i;
-	int	num;
+	int	result;
+	int	sign;
 
-	num = 0;
-	i = -1;
-	while (str[++i])
+	result = 0;
+	sign = 1;
+	while (ft_isspace(*str))
+		str++;
+	if (*str == '+' || *str == '-')
 	{
-		num *= 10;
-		num = str[i] - '0';
+		if (*str == '-')
+			sign *= -1;
 	}
-	return (num);
+	while (*str >= '0' && *str <= '9')
+	{
+		result = (result * 10) + (*str - '0');
+		str++;
+	}
+	return (result * sign);
+}
+
+int	ft_error(char *s)
+{
+	printf("%s\n", s);
+	return (-1);
+}
+
+void	ft_printf(t_game *game, char *str, int id)
+{
+	pthread_mutex_lock(&(game->write));
+	if (!(game->die))
+	{
+		printf("%lld ", ft_time() - game->start_time);
+		printf("%d ", id + 1);
+		printf("%s\n", str);
+	}
+	pthread_mutex_unlock(&(game->write));
+	return ;
 }
