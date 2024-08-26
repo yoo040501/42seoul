ce4e40c66bba3da02fa5511d75e824cb6c0b694e
felix <90296853+yoo040501@users.noreply.github.com>
2024-08-26 22:49:58 +0900

    Update HumanB.cpp

--- cpp_module/cpp01/ex03/HumanB.cpp
+++ cpp_module/cpp01/ex03/HumanB.cpp
@@ -8,11 +8,11 @@ HumanB::HumanB(std::string name) : name(name)
 HumanB::~HumanB(void)
 {}
 
-void	HumanB::setWeapon(Weapon weapon)
+void	HumanB::setWeapon(Weapon &weapon)
 {
-	this->weapon = weapon;
+	this->weapon = &weapon;
 }
 
 void	HumanB::attack(void){
-	std::cout << this->name << " attacks with their " << this->weapon.getType() << '\n';
+	std::cout << this->name << " attacks with their " << this->weapon->getType() << '\n';
 }
