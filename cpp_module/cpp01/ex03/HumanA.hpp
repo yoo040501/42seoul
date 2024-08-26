80715d32bf9ae876f8df3b033538dd7b00a262e2
felix <90296853+yoo040501@users.noreply.github.com>
2024-08-26 22:48:33 +0900

    Update HumanA.hpp

--- cpp_module/cpp01/ex03/HumanA.hpp
+++ cpp_module/cpp01/ex03/HumanA.hpp
@@ -5,9 +5,9 @@
 
 class HumanA{
 	std::string	name;
-	Weapon		weapon;
+	Weapon		&weapon;
 	public:
-		HumanA(std::string name, Weapon weapon);
+		HumanA(std::string name, Weapon &weapon);
 		~HumanA(void);
 		void attack(void);
 };
