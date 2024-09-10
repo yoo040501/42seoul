241bd5533013dc038f906ac68aff0487f987cdab
felix <90296853+yoo040501@users.noreply.github.com>
2024-09-11 00:10:33 +0900

    Update Ice.hpp

--- cpp_module/cpp04/ex03/Ice.hpp
+++ cpp_module/cpp04/ex03/Ice.hpp
@@ -15,10 +15,36 @@ class Ice : public AMateria
 		void		use(ICharacter& target);
 };
 
-Ice::Ice(/* args */)
+Ice::Ice() : AMateria("ice")
 {
+    // std::cout << "* Ice: materia is created *" << std::endl;
+}
+
+Ice::Ice( Ice const & src ) : AMateria("ice")
+{
+    *this = src;
+    // std::cout << "* Ice: materia is copied *" << std::endl;
+}
+
+Ice&   Ice::operator=( const Ice& rhs ) {
+    if (this != &rhs)
+    {
+        this->_type = rhs._type;
+    }
+    return *this;
 }
 
 Ice::~Ice()
 {
+    // std::cout << "* Ice: ice is destroyed *" << std::endl;
+}
+
+AMateria* Ice::clone() const
+{
+    return (new Ice(*this));
+}
+
+void    Ice::use( ICharacter& target )
+{
+    std::cout << "I shoots an ice bolt at " << target.getName() << std::endl;
 }
