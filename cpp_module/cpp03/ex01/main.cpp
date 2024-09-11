81f3230d0d00b4fb671436e91c75a8f8fd1e57f4
felix <90296853+yoo040501@users.noreply.github.com>
2024-09-11 21:46:09 +0900

    Update main.cpp

--- cpp_module/cpp03/ex01/main.cpp
+++ cpp_module/cpp03/ex01/main.cpp
@@ -15,4 +15,32 @@ int	main(void)
 	}
 	C.attack("A");
 	B.attack("A");
+
+	std::cout << "-----------TESTING CLAPTRAP-----------------\n" << std::endl;
+	{
+		ClapTrap a;
+		ClapTrap b("b");
+
+		for (int i = 0; i < 12; i++)
+			b.attack("Cody-clone");
+		b.beRepaired(3);
+	}
+	std::cout << "\n-------------------TESTING SCAVTRAP------------------\n" << std::endl;
+	{
+		ScavTrap c;
+		ScavTrap d("d");
+
+		c.attack("CloneTrap");
+		// for (int i = 0; i < 50; i++)
+		// 	c.attack("CloneTrap");
+		c.beRepaired(22);
+		c.takeDamage(21);
+		c.beRepaired(22);
+		c.guardGate();
+		c.guardGate();
+		d.attack("Savage-clone");
+		d.takeDamage(101);
+		d.takeDamage(15);
+		d.attack("ScavTrap-clone");
+	}
 }
