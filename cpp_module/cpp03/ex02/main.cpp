511fc83ce7787a9b27f350d4d958c80664144787
felix <90296853+yoo040501@users.noreply.github.com>
2024-09-11 21:47:08 +0900

    Update main.cpp

--- cpp_module/cpp03/ex02/main.cpp
+++ cpp_module/cpp03/ex02/main.cpp
@@ -15,4 +15,20 @@ int	main(void)
 	}
 	C.attack("A");
 	B.attack("A");
+
+	std::cout << "\n\n-------------TESTING FRAGTRAP---------------------\n" << std::endl;
+	{
+		FragTrap e;
+		FragTrap f("Chadd");
+
+		std::cout << "\033[34mTesting\033[0m" << std::endl;
+		e.highFiveGuys();
+		e.attack("some random dude");
+		e.takeDamage(101);
+		e.takeDamage(1);
+		e.attack("some random dude");
+		f.highFiveGuys();
+		// for(int i = 0; i < 101; i++)
+		// 	f.attack("FragTrap-clone");
+	}
 }
