94684c2d2a54b615ec8da5db12078ba914e2c3d1
felix <90296853+yoo040501@users.noreply.github.com>
2024-09-15 17:18:04 +0900

    Update MateriaSource.cpp

--- cpp_module/cpp04/ex03/MateriaSource.cpp
+++ cpp_module/cpp04/ex03/MateriaSource.cpp
@@ -9,9 +9,32 @@ MateriaSource::MateriaSource(const MateriaSource& other): IMateriaSource(other){
 }
 
 void	MateriaSource::learnMateria(AMateria *){
-
+		for (int i = 0; i < 4; i++)
+	{
+		if (materia && this->_learnInventory[i] == NULL)
+		{
+			if (this->inLearnInventory(materia))
+				this->_learnInventory[i] = materia;
+			else
+				this->_learnInventory[i] = materia;
+			std::cout << "Materia " << this->_learnInventory[i]->getType() << " learned at index " << i << std::endl;
+			return ;
+		}
+	}
+	if (materia)
+		std::cout << "Cannot learn materia, inventory for MateriaSource is full!" << std::endl;
+	else
+		std::cout << "Cannot learn invalid materia" << std::endl;
+	if (!this->inLearnInventory(materia))
+		delete materia;
 }
 
 AMateria*	MateriaSource::createMateria(std::string const& type){
-
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->_learnInventory[i] && this->_learnInventory[i]->getType() == type)
+			return (this->_learnInventory[i]->clone());
+	}
+	std::cout << "Cannot create materia, " << type << " is invalid!" << std::endl;
+	return (0);
 }
