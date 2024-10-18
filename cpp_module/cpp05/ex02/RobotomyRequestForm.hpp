a03b7b33bbbb93725bf10858b7c446cd5a538979
felix <90296853+yoo040501@users.noreply.github.com>
2024-10-18 20:28:44 +0900

    Update RobotomyRequestForm.hpp

--- cpp_module/cpp05/ex02/RobotomyRequestForm.hpp
+++ cpp_module/cpp05/ex02/RobotomyRequestForm.hpp
@@ -6,10 +6,15 @@
 class RobotomyRequestForm : public AForm
 {
 private:
-	/* data */
+	const std::string _target;
 public:
 	RobotomyRequestForm();
+	RobotomyRequestForm(std::string target);
 	RobotomyRequestForm(const RobotomyRequestForm& R);
 	RobotomyRequestForm& operator=(const RobotomyRequestForm& copy);
+
+	void execute(Bureaucrat const &executor)const;
+	std::string getTarget(void)const;
 	~RobotomyRequestForm();
 };
+std::ostream	&operator<<(std::ostream &o, RobotomyRequestForm *a);
