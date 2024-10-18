0c535bb05e65a07a6c359742f4dafed700ca5c60
felix <90296853+yoo040501@users.noreply.github.com>
2024-10-18 20:21:23 +0900

    Update main.cpp

--- cpp_module/cpp05/ex00/main.cpp
+++ cpp_module/cpp05/ex00/main.cpp
@@ -1,9 +1,41 @@
 #include "Bureaucrat.hpp"
 
 int main(void){
-	Bureaucrat	a("a", 500);
-	Bureaucrat	b("b", 123);
 
-	a.decrementGrade(5);
-	std::cout << a << std::endl;
+	try
+	{
+		try
+		{
+			Bureaucrat test("test", 200);	
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		
+		Bureaucrat a("a", 150);
+		Bureaucrat b("b", 1);
+
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << std::endl;
+		
+		// a.decrementGrade();
+	
+		// b.incrementGrade();
+	
+		//std::cout << a << std::endl;
+		//std::cout << b << std::endl;
+		//std::cout << std::endl;
+
+		a.incrementGrade();
+		b.decrementGrade();
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
