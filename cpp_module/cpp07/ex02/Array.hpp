26d88144eaa874dbc84ca860eff910cbe681d5df
felix <90296853+yoo040501@users.noreply.github.com>
2024-11-06 21:29:40 +0900

    Update Array.hpp

--- cpp_module/cpp07/ex02/Array.hpp
+++ cpp_module/cpp07/ex02/Array.hpp
@@ -36,14 +36,78 @@
 // 마지막으로, 모든 기능이 예상대로 작동하는지 확인하고, 테스트를 포함한 main.cpp 파일을 제출하라고 요구하고 있습니다.
 
 template <typename T>
-class Array
-{
-	private:
-		T	*arr;
-	public:
-		Array();
-		Array(unsigned int n);
-		Array(Array const& A);
-		Array& operator=(Array const& copy);
-		~Array();
+template <typename T>
+class Array {
+ private:
+	std::size_t len;
+	T *array;
+ public:
+	Array(void);
+	Array(std::size_t n);
+	Array(const Array& obj);
+	Array& operator=(const Array& obj);
+	~Array(void);
+	std::size_t size(void) const;
+	T& operator[] (std::size_t i);
+	const T& operator[] (std::size_t i) const;
 };
+
+# include "Array.tpp"
+
+template <typename T>
+Array<T>::Array() : _size(0), _data(NULL) {}
+
+template <typename T>
+Array<T>::Array(uint size) : _size(size), _data(new T[size]()) {}
+
+template <typename T>
+Array<T>::Array(const Array<T>& other)
+    : _size(other._size), _data(new T[other._size]) {
+  for (size_t i = 0; i < _size; i++)
+    _data[i] = other._data[i];
+}
+
+template <typename T>
+Array<T>::~Array() {
+  delete[] _data;
+}
+
+// Operators
+template <typename T>
+static void swap(T& a, T& b) {
+  T temp = a;
+  a = b;
+  b = temp;
+}
+
+template <typename T>
+Array<T>& Array<T>::operator=(const Array<T>& other) {
+  Array tmp(other);
+  swap(_size, tmp._size);
+  swap(_data, tmp._data);
+  return *this;
+}
+
+template <typename T>
+inline bool Array<T>::is_valid_index(size_t index) const {
+  return (0 <= index && index < _size);
+}
+
+template <typename T>
+const T& Array<T>::operator[](size_t index) const {
+  if (not is_valid_index(index))
+    throw std::out_of_range("(value) Index out of range");
+  return _data[index];
+}
+
+template <typename T>
+T& Array<T>::operator[](size_t index) {
+  if (not is_valid_index(index))
+    throw std::out_of_range("(reference) Index out of range");
+  return _data[index];
+}
+
+template <typename T>
+size_t Array<T>::size() const {
+  return _size;
+}
