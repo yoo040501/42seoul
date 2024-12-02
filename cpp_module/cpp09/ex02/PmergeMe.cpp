ac37a65999f1bbb4c92ff0e3dd69baad527486b3
felix <90296853+yoo040501@users.noreply.github.com>
2024-12-02 22:49:56 +0900

    Update PmergeMe.cpp

--- cpp_module/cpp09/ex02/PmergeMe.cpp
+++ cpp_module/cpp09/ex02/PmergeMe.cpp
@@ -43,3 +43,88 @@ void	PmergeMe::getNum(char *str)
 	else
 		num.push_back(number);
 }
+
+
+
+void PmergeMe::mergeVec(int start, int mid, int end)
+{
+    int left_size = mid - start + 1;
+    int right_size = end - mid;
+    std::vector<int> left(left_size);
+    std::vector<int> right(right_size);
+
+    int i = 0;
+    while (i < left_size)
+    {
+        left[i] = this->vec[start + i];
+        i++;
+    }
+    int j = 0;
+    while (j < right_size)
+    {
+        right[j] = this->vec[mid + 1 + j];
+        j++;
+    }
+    i = 0;
+    j = 0;
+    int idx = start;
+    while (i < left_size && j < right_size)
+    {
+        if (left[i] <= right[j])
+        {
+            this->vec[idx] = left[i];
+            i++;
+        }
+        else
+        {
+            this->vec[idx] = right[j];
+            j++;
+        }
+        idx++;
+    }
+
+    while (i < left_size)
+    {
+        this->vec[idx] = left[i];
+        i++;
+        idx++;
+    }
+
+    while (j < right_size)
+    {
+        this->vec[idx] = right[j];
+        j++;
+        idx++;
+    }
+}
+
+void PmergeMe::InsertVec(int start, int end)
+{
+    for (int i = start+1; i <= end; i++)
+    {
+        int key = this->vec[i];
+        int j = i - 1; 
+        while (j >= start && this->vec[j] > key)
+        {
+            this->vec[j + 1] = this->vec[j];
+            j--;
+        }
+        this->vec[j + 1] = key;
+    }
+}
+
+void PmergeMe::mergeInsertVec(int start, int end)
+{
+    if (start < end)
+    {
+        if (end - start <= 10)
+            InsertVec(start, end);
+        else
+        {
+            int mid = start + (end - start) / 2;
+            mergeInsertVec(start, mid);
+            mergeInsertVec(mid+1, end);
+            mergeVec(start, mid, end);
+        }
+    }
+}
