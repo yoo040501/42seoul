cb4bb1dab8946572ef452f2a248a7a9f250f858e
yoo040501 <yoo040501@naver.com>
2025-02-24 18:12:37 +0900

    inception finish

---
+++ inception/srcs/requirements/mariadb/tools/init.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+sed -i 's/^bind-address.*/# bind-address  = 127.0.0.1/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+mkdir -p /run/mysqld && chown -R mysql:mysql /run/mysqld
+mkdir -p /var/lib/mysql
+chown -R mysql:mysql /var/lib/mysql
+chmod 755 /var/lib/mysql
+
+# MariaDB 데이터 디렉토리가 비어 있을 때만 초기화 실행
+if [ ! -d /var/lib/mysql/$MYSQL_DATABASE ]; then
+    echo "Initializing database..."
+
+    mysqld --user=mysql --skip-networking &
+    MYSQLD_PID=$!
+
+    for i in {30..0}; do
+        if echo 'SELECT 1' | mysql --protocol=socket --socket=/run/mysqld/mysqld.sock -uroot &> /dev/null; then
+            break
+        fi
+        sleep 1
+    done
+
+    if [ "$i" = 0 ]; then
+        echo "MariaDB did not start in time" >&2
+        exit 1
+    fi
+
+    mysql --protocol=socket -uroot <<-EOF
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+        GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+        FLUSH PRIVILEGES;
+EOF
+
+    kill -s TERM "$MYSQLD_PID"
+    wait "$MYSQLD_PID"
+else
+    echo "Database already initialized, skipping initialization."
+fi
+
+exec mysqld --user=mysql
---
+++ inception/srcs/requirements/wordpress/tools/script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sed -i 's/^listen = .*/listen = 0.0.0.0:9000/' /etc/php/7.4/fpm/pool.d/www.conf
+mkdir -p /run/php && chown -R www-data:www-data /run/php
+
+if [ ! -f "$WP_PATH/wp-config.php" ]; then
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	mv wp-cli.phar /usr/local/bin/wp
+    wp core download --path=$WP_PATH --allow-root
+
+	wp config create \
+		--dbname="$WORDPRESS_DB_NAME" \
+		--dbuser="$WORDPRESS_DB_USER" \
+		--dbpass="$WORDPRESS_DB_PASSWORD" \
+		--dbhost="$WORDPRESS_DB_HOST" \
+		--path=$WP_PATH --allow-root
+	wp core install --url="$WORDPRESS_URL" \
+					--title="$WORDPRESS_TITLE" \
+					--admin_user="$ADMIN_USER" \
+					--admin_password="$ADMIN_PASSWORD" \
+					--admin_email="$ADMIN_EMAIL" \
+					--path=$WP_PATH --allow-root
+					
+	wp user create user1 user1@gmail.com --role=subscriber --user_pass="$ADMIN_PASSWORD" --path="$WP_PATH" --allow-root
+	wp plugin install akismet --activate --path="$WP_PATH" --allow-root
+	wp theme install twentytwentyfour --activate --path="$WP_PATH" --allow-root
+fi
+
+/usr/sbin/php-fpm7.4 -F
